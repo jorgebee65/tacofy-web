9656952c3abfced66406e26af84f378502c69f87
Jorge Dominguez <jorgebee65@hotmail.com>
2018-07-30 15:53:58 -0500

    Agrego Cards de Taquerias

--- vuejsfirebase/src/App.vue
+++ vuejsfirebase/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
       <div class="container">
@@ -13,6 +14,38 @@
     <div class="parallax tinted">
       <img src="./assets/background1.jpg">
     </div>
+  </div>
+  <div class="container">
+      <div class="row">
+          <form class="col s12">
+            <div class="row">
+                <div class="input-field col s12 m4">
+                  <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                  <label for="first_name">Taqueria</label>
+                </div>
+                <div class="input-field col s12 m6">
+                  <input id="last_name" type="text" class="validate">
+                  <label for="last_name">Tipo de Taco</label>
+                </div>
+                <a class="waves-effect waves-light btn amber">Buscar</a>
+            </div>
+          </form>
+      </div>
+          <div class="row" >
+            <div class="col s12 m3" v-for="taq in taquerias">
+              <div class="card">
+                <div class="card-image">
+                  <img v-bind:src="taq.imagen" />
+                  <span class="card-title">{{taq.nombre}}</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                  <p>{{taq.ubicacion}}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+  </div>
   </div>
 </template>
 
@@ -40,7 +73,9 @@ export default {
   },
   mounted: function(){
     getTaquerias().then(taquerias => this.taquerias = taquerias)
-    $('.parallax').parallax();
+    $('.parallax').parallax()
+    //$('.sidenav').sidenav()
+    $(".dropdown-trigger").dropdown()
   }
 }
 
